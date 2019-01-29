f03e322cec7e6cada381af3967846ecd245c4b2f
Aditya Pratap Singh <45251408+adityacryptota@users.noreply.github.com>
2019-01-29 17:39:16 +0530

    V1

---
+++ app.js
@@ -0,0 +1,31 @@
+let container = document.querySelector('.container');
+
+let small = document.querySelector('small');
+
+let hr = document.querySelector('hr');
+
+let p = document.querySelector('p');
+
+let button = document.querySelector('.button');
+
+let icons = document.querySelector('.icons');
+
+setTimeout(() => {
+    small.style.visibility = 'visible';
+}, 2000);
+
+setTimeout(() => {
+    hr.style.visibility = 'visible';
+}, 3000);
+
+setTimeout(() => {
+    p.style.visibility = 'visible';
+}, 4000);
+
+setTimeout(() => {
+    button.style.visibility = 'visible';
+}, 5000);
+
+setTimeout(() => {
+    icons.style.visibility = 'visible';
+}, 5000);
